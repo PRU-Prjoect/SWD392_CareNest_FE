3df8158872fb111e7d8e0f4f3f012109be85938f
Gia Bảo Huỳnh <baohgse182507@fpt.edu.vn>
2025-06-17 16:05:16 +0700

    Add
    Thêm presist vào để load lại trang không bị logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 // src/App.tsx
+import { PersistGate } from "redux-persist/integration/react";
 import { AuthWrapper } from "./pages/Login/components/AuthWrapper";
 import AppRoutes from "./routes/index";
+import { persistor } from "./store/store";
 
 function App() {
   return (
-    <AuthWrapper>
-      <AppRoutes />
-    </AuthWrapper>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <AuthWrapper>
+        <AppRoutes />
+      </AuthWrapper>
+    </PersistGate>
   );
 }
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,28 +1,41 @@
 // store/store.ts
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import registeReducer from "./slices/registerSlice";
 import registerCustomerReducer from "./slices/registerCustomerSlice";
-import registerShopReducer from "./slices/registerShopSlice"; 
+import registerShopReducer from "./slices/registerShopSlice";
 import customerReducer from "./slices/customerSlice";
 import shopReducer from "./slices/shopSlice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"], // chỉ persist auth slice
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  register: registeReducer,
+  registerCustomer: registerCustomerReducer,
+  registerShop: registerShopReducer,
+  customer: customerReducer,
+  shop: shopReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
- reducer: {
-    auth: authReducer,
-    register: registerReducer,
-    registerCustomer: registerCustomerReducer,
-    registerShop: registerShopReducer,
-    customer: customerReducer,
-    shop: shopReducer,
- },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: ["persist/PERSIST"],
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
       },
     }),
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
